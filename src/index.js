64c949979d7defe85d65302bdf74a34239183c12
Néstor Argüello <nestor.arguello@gmail.com>
2018-08-05 20:40:57 -0400

    add Layout component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import App from './App';
+import { injectGlobal } from 'styled-components';
 import registerServiceWorker from './registerServiceWorker';
+
+injectGlobal`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Open Sans', sans-serif;
+  }
+`;
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
